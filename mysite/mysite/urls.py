dadb4a2b8ee1d5ef89eea96435b5ba48e78c632c
JCMZZ <jiangchunmingzz@gmail.com>
2018-07-19 14:42:37 +0800

    自定义工程模板

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.views import serve
+
 
 urlpatterns = [
+    path('favicon.ico', serve, {'path': '/favicon.ico'}),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
